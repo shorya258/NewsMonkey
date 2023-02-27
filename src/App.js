501e1da92c665409fcf18a311be325c57bcbe9f9
Shorya Hayaran <Shoryahayaran@gmail.com>
2023-02-27 20:15:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from "./components/NavBar";
+import React, { Component } from "react";
+import News from "./components/News";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Temp from "./components/Temp";
+//news api key f25897909b79440195c4847071c6a739
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <Route path="/general" key="general">
+            <News country="in" pageSize={15} category="general" />
+          </Route>
+          <Route path={"/business"} key="business">
+            <News country="in" pageSize={15} category="business" />
+          </Route>
+          <Route path={"/entertainment"} key="entertainment">
+            <News country="in" pageSize={15} category="entertainment" />
+          </Route>
+          <Route path={"/health"} key="health">
+            <News country="in" pageSize={15} category="health" />
+          </Route>
+          <Route path={"/science"} key="science">
+            <News country="in" pageSize={15} category="science" />
+          </Route>
+          <Route path={"/sports"} key="sports">
+            <News country="in" pageSize={15} category="sports" />
+          </Route>
+          <Route path="/technology" key="technology">
+            <News country="in" pageSize={15} category="technology" />
+          </Route>
+          <Route exact path="/" key="general">
+            <News country="in" pageSize={15} category="general" />
+          </Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
-export default App;
+// export default App;
---
+++ src/components/Temp.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Temp extends Component {
+  render() {
+    return <div>Temp</div>;
+  }
+}
+
+export default Temp;
